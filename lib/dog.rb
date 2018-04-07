3fcd2d033fb83ada503efbd89a0ec83a4459ce75
Michael Kang <michaelkang85@gmail.com>
2018-04-07 18:35:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,7 +47,7 @@ attr_accessor :name, :breed, :id
   end
 
   def self.create(name: breed:)
-    new_dog = Dog.new(name:name, breed:breed)
+    new_dog = Dog.new(name, breed)
     new_dog.save
     new_dog
   end
