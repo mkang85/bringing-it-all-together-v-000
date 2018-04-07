4049e68cdc6299b51e37aeec0186936ff208678a
Michael Kang <michaelkang85@gmail.com>
2018-04-07 18:07:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,7 +52,7 @@ attr_accessor :name, :breed, :id
   def self.find_or_create_by(name:, breed:)
   dog_data = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)[0]
     if !dog_data.nil?
-      dog = self.new(id:dog_data[0], name:dog_data[1], breed:dog_data[2])
+      dog = self.new_from_db(dog_data)
     else
       dog = self.create(name:name, breed:breed)
     end
