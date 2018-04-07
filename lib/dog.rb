5bb9d87478a54f930001a5909b4c6cfde8ab47a3
Michael Kang <michaelkang85@gmail.com>
2018-04-07 15:49:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,6 +7,15 @@ attr_accessor :name, :breed, :id
     @breed = breed
   end
 
-  
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY, 
+      name TEXT, 
+      breed TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end 
 
 end
