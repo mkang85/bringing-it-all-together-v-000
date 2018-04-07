d9748da9b512651825757b09f54d6a75ad8fb241
Michael Kang <michaelkang85@gmail.com>
2018-04-07 16:42:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,7 +61,7 @@ attr_accessor :name, :breed, :id
     WHERE id = ?
     SQL
     result = DB[:conn].execute(sql, id)
-    binding.pry
+    #binding.pry
     new_dog = Dog.new(id:result[0], name:result[1], breed:result[2])
     new_dog.save
   end
