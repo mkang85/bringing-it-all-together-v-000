dc650a16d82fc72c4506472c38320e1c0658aaeb
Michael Kang <michaelkang85@gmail.com>
2018-04-07 17:50:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,7 +53,7 @@ attr_accessor :name, :breed, :id
   #binding.pry
     if !dog_data == nil
       # stuff = dog_data[0]
-      dog = self.new_from_db(dog_data)
+      dog = self.new(id:dog_data[0], name:dog_data[1], breed:dog_data[2])
     else
       dog = self.create(name:name, breed:breed)
     end
