2940ba33f662a318fd4283924d5767d5009ea19f
Michael Kang <michaelkang85@gmail.com>
2018-04-07 17:14:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -71,4 +71,13 @@ attr_accessor :name, :breed, :id
     new_dog
   end
 
+  def update 
+    sql =<<-SQL
+    SELECT * 
+    FROM dogs
+    WHERE name = ? AND album = ?
+    SQL
+    DB[:conn].execute(sql)
+  end 
+
 end
