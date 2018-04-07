458838156597d0ecc163224bc23ad6fa42c69749
Michael Kang <michaelkang85@gmail.com>
2018-04-07 18:24:19 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog 
+attr_accessor :name, :breed, :id 
+
+  def initialize(id:, name:, breed:)
+    @id = id 
+    @name = name 
+    @breed = breed 
+  end 
+
+  def self.create_table 
+    sql =<<-SQL
+    CREATE TABLE IF NOT EXISTS dogs(
+      id INTEGER PRIMARY KEY 
+      name TEXT, 
+      breed TEXT
+    )
+    SQL
+    DB[:conn].execute("CREATE TABLE IF NOT EXISTS dogs")
+  end 
+
+  def self.drop_table
+  end 
+end 
