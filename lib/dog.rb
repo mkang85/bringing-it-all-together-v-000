0aa4538b4e661b725e47b09aff90cad7ac0835f6
Michael Kang <michaelkang85@gmail.com>
2018-04-07 16:17:56 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -35,7 +35,8 @@ attr_accessor :name, :breed, :id
       INSERT INTO dogs (name, breed)
       VALUES (?, ?)
       SQL
-      DB[:conn].execute(sql, name:self.name, breed:self.breed)
+      DB[:conn].execute(sql, self.name, self.breed)
+      binding.pry
       @id = DB[:conn].execute("SELECT last_insert_row_id() FROM dogs")[0][0]
     end
   end
