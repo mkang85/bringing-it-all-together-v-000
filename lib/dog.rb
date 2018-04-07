8e767ec011a01883cccc6d8610a283091947a03b
Michael Kang <michaelkang85@gmail.com>
2018-04-07 16:41:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,7 +72,6 @@ attr_accessor :name, :breed, :id
     WHERE name = ?
     SQL
     result = DB[:conn].execute(sql,name)[0]
-    binding.pry
     new_dog = Dog.new(id:result[0], name:result[1], breed:result[2])
 
   end
