f258b7b8d041fb3bca66b4c3283b2fc5cf649ca3
Michael Kang <michaelkang85@gmail.com>
2018-04-07 17:23:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,7 +53,7 @@ attr_accessor :name, :breed, :id
     if !dog_data.empty?
       dog = self.new_from_db(dog_data)
     else
-      dog = self.create(name:, breed:)
+      dog = self.create(name:name, breed:breed)
     end
     dog
   end
